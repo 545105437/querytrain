6ed40c7682b771695199da17629ed12dc74c29db
wal <545105437@qq.com>
2018-08-24 17:13:26 +0800

    添加获取真实ip的方法，防止因代理等原因导致找不到真实ip的问题

--- src/main/java/com/train/aop/SearchLogHistoryAop.java
+++ src/main/java/com/train/aop/SearchLogHistoryAop.java
@@ -4,6 +4,7 @@ import com.train.annotation.AnSearchLogHistory;
 import com.train.model.SearchLogHistory;
 import com.train.service.SearchLogHistoryService;
 import com.xiaoleilu.hutool.date.DateTime;
+import org.apache.commons.lang.StringUtils;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -39,8 +40,10 @@ public class SearchLogHistoryAop {
 	@Before("cut()")
 	public void doBefore(JoinPoint joinPoint){
 
+	    //获取拦截的方法对象
 		MethodSignature sign =  (MethodSignature)joinPoint.getSignature();
 		Method method = sign.getMethod();
+		//获取annotation对象
 		AnSearchLogHistory annotation = method.getAnnotation(AnSearchLogHistory.class);
 
 		ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
@@ -52,11 +55,44 @@ public class SearchLogHistoryAop {
 		searchLogHistory.setActionType(annotation.actiontype());//操作类型
 		searchLogHistory.setAction(annotation.action());//操作描述
 		searchLogHistory.setUrl(String.valueOf(request.getRequestURL()));//url
-		searchLogHistory.setIp(request.getRemoteAddr());//操作人IP地址
+        /** 获取访问者真实ip */
+        String ip = getIpAddr(request);
+        searchLogHistory.setIp(request.getRemoteAddr());//操作人IP地址
 
 		//录入数据库
 		SearchLogHistory history = searchLogHistoryService.save(searchLogHistory);
 		logger.info("成功记录操作数据:"+history);
 	}
+
+    /**
+     * 获取访问者IP
+     *
+     * 在一般情况下使用Request.getRemoteAddr()即可，但是经过nginx等反向代理软件后，这个方法会失效。
+     *
+     * 本方法先从Header中获取X-Real-IP，如果不存在再从X-Forwarded-For获得第一个IP(用,分割)，
+     * 如果还不存在则调用Request .getRemoteAddr()。
+     *
+     * @param request
+     * @return
+     */
+    public  String getIpAddr(HttpServletRequest request) {
+
+        String ip = request.getHeader("X-Real-IP");
+        if (!StringUtils.isBlank(ip) && !"unknown".equalsIgnoreCase(ip)) {
+            return ip;
+        }
+        ip = request.getHeader("X-Forwarded-For");
+        if (!StringUtils.isBlank(ip) && !"unknown".equalsIgnoreCase(ip)) {
+            // 多次反向代理后会有多个IP值，第一个为真实IP。
+            int index = ip.indexOf(',');
+            if (index != -1) {
+                return ip.substring(0, index);
+            } else {
+                return ip;
+            }
+        } else {
+            return request.getRemoteAddr();
+        }
+    }
 
 }
